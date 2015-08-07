20df2f575880a6a084aa726e89b090909f0d8900
ykorostelyov <ykorostelyov@gmail.com>
2015-08-07 13:25:44 +0300

    ticket:1 fixing server error

--- fortytwo_test_task/wsgi.py
+++ fortytwo_test_task/wsgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fortytwo_test_task.settings")
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
