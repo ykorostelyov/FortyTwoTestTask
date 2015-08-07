628684b6fc8aa7ba9c49e47802b3e2286cfef1a4
ykorostelyov <ykorostelyov@gmail.com>
2015-08-07 12:53:30 +0300

    ticket:1 fixing server error

--- apps/Mydata/views.py
+++ apps/Mydata/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
 from .models import Mycard
+from .models import RequestInfo
 import os
 
 
@@ -7,13 +9,10 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
 # base dir
 B_DIR = os.path.dirname(PROJECT_DIR)
 
-# list of records
-all_result = Mycard.objects.all()
-# first record
-frst_result = all_result.filter()[:1]
-
 
 # index
 def index(request):
     full_path = os.path.join(B_DIR+"/apps/Mydata/templates/Mydata/index.html")
+    all_result = Mycard.objects.all()
+    frst_result = all_result.filter()[:1]
     return render_to_response(full_path, {'frst_result': frst_result})
