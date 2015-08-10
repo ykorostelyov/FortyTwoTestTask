941b5acce8a55c3d52b153f93ec215d9e2e5c659
ykorostelyov <ykorostelyov@gmail.com>
2015-08-10 22:18:37 +0300

    ticket:1 some

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -6,15 +6,15 @@ class TestMycardModel(TestCase):
     Skype_str = "yuriy.torhammer"
 
     def setUp(self):
-        Mycard.objects.create(fName='Yuriy',
-                              lName='Korostelyov',
-                              Skype=self.Skype_str,
-                              bDate="1983-01-13",
-                              Jabber='ykorostelyov@khavr.com'
+        Mycard.objects.create(first_name='Yuriy',
+                              last_name='Korostelyov',
+                              skype=self.Skype_str,
+                              birth_date="1983-01-13",
+                              jabber='ykorostelyov@khavr.com'
                               )
 
     def test_person(self):
         """Testing of My card creatig"""
         mycard = Mycard.objects.get(id='1')
-        self.assertEqual(mycard.Skype, self.Skype_str)
-        self.assertEqual(mycard.Jabber, 'ykorostelyov@khavr.com')
+        self.assertEqual(mycard.skype, self.Skype_str)
+        self.assertEqual(mycard.jabber, 'ykorostelyov@khavr.com')
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render_to_response
 from .models import Mycard
-import os
-
-
-PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
-# base dir
-B_DIR = os.path.dirname(PROJECT_DIR)
 
 
 # index
 def index(request):
-    all_result = Mycard.objects.all()
-    frst_result = all_result.filter()[:1]
-    return render_to_response("hello/index.html", {'frst_result': frst_result})
+    first_result = Mycard.objects.order_by('id')[0]
+    return render_to_response("hello/index.html", {'first_result': first_result})
