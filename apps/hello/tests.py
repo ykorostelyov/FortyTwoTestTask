e588bc224d4afe17c852819c7c36ad5bdb5c1998
ykorostelyov <ykorostelyov@gmail.com>
2015-08-11 18:41:32 +0300

    ticket:1 fixing some observations

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -14,6 +14,7 @@ class TestMycardModel(TestCase):
         self.assertIsNotNone(mycard)
 
     def test_is_init_data_correct(self):
+        """Testing correcting of init data"""
         # Is All properties included
         mycard = Mycard.objects.get(id=1)
         self.assertIsNotNone(mycard)
@@ -27,7 +28,8 @@ class TestMycardModel(TestCase):
         # Is data correct
         self.assertEqual(mycard.skype, self.Skype_str)
         self.assertEqual(mycard.jabber, 'ykorostelyov@khavr.com')
-        self.assertEqual(mycard.first_name+' '+mycard.last_name, 'Yuriy Korostelyov')
+        self.assertEqual(mycard.first_name+' '+mycard.last_name,
+                         'Yuriy Korostelyov')
         self.assertNotEqual(len(mycard.bio), 0)
         self.assertNotEqual(len(mycard.other_contacts), 0)
         self.assertNotEqual(mycard.birth_date, datetime.date.today())
@@ -41,6 +43,7 @@ class TestMycardModel(TestCase):
         self.assertEqual(mycard, 1)
 
     def test_is_data_model_presented_in_db(self):
+        """Testing of site structure"""
         # Getting default page
         response = self.client.get('')
         # Is current response valid
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -12,7 +12,9 @@ def index(request):
     # Check for Model is present
     mycard = get_object_or_404(Mycard, id=1)
     log.info('Getting data of model for homepage')
-    log.debug('record for ' + mycard.first_name + ' ' + mycard.last_name + ' person')
+    log.debug('rec for ' + mycard.first_name +
+              ' ' + mycard.last_name + ' person')
     log.debug('Displayng russian characters. bio = ' + mycard.bio)
     first_result = Mycard.objects.order_by('id')[0]
-    return render_to_response("hello/index.html", {'first_result': first_result})
+    return render_to_response("hello/index.html",
+                              {'first_result': first_result})
