0ffba95266efd66f37d03fd6997b7691cf96663d
ykorostelyov <ykorostelyov@gmail.com>
2015-08-11 21:06:59 +0300

    ticket:1 fixing some observations

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -9,16 +9,6 @@ log = logging.getLogger('apps')
 
 # index
 def index(request):
-    # Check for Model is present
-    # try:
-    #     mycard = Mycard.objects.get(id=1)
-    # except Mycard.DoesNotExist:
-    #     raise Http404
-
-    # log.info('Getting data of model for homepage')
-    # log.debug('record for ' + mycard.first_name +
-    #           ' ' + mycard.last_name + ' person')
-    # log.debug('Displayng russian characters. bio = ' + mycard.bio)
     first_result = Mycard.objects.order_by('id')[0]
     return render_to_response("hello/index.html",
                               {'first_result': first_result})
