edb45ac6cfd4376d08ab6682886ae78eb308630c
ykorostelyov <ykorostelyov@gmail.com>
2015-08-12 11:54:03 +0300

    ticket:1 back to worked version

--- manage.py
+++ manage.py
@@ -9,8 +9,3 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-
-
-if '--noinput' in sys.argv:
-    if not '--all' in sys.argv:
-        os.system('python manage.py syncdb --noinput --all')
