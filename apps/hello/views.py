559c333b87f1dc252682fc3048a4d62fc1c12b65
ykorostelyov <ykorostelyov@gmail.com>
2015-08-12 11:39:43 +0300

    ticket:1 back to worked version

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,24 +1,8 @@
 from django.shortcuts import render_to_response
 from .models import Mycard
-from django.http import Http404
-import logging
 
 
-log = logging.getLogger('apps')
-
-
-# index
 def index(request):
-    try:
-        mycard = Mycard.objects.get(id=1)
-    except Mycard.DoesNotExist:
-        raise Http404
-
-    log.info('Getting data of model for homepage')
-    log.debug('record for ' + mycard.first_name +
-              ' ' + mycard.last_name + ' person')
-    log.debug('Displayng russian characters. bio = ' + mycard.bio)
-
-    first_result = Mycard.objects.order_by('id')[0]
-    return render_to_response("hello/index.html",
-                              {'first_result': first_result})
+    all_result = Mycard.objects.all()
+    frst_result = all_result.filter()[:1]
+    return render_to_response("hello/index.html", {'frst_result': frst_result})
