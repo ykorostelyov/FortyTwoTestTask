ea8d44ec818f35644ccfe7b6d4939cf76ddad289
ykorostelyov <ykorostelyov@gmail.com>
2015-08-27 13:13:54 +0300

    ticket:1 fixing all observations from previous test

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 from .models import Mycard
 import datetime
 
 
 class TestMycardModel(TestCase):
-    Skype_str = "yuriy.torhammer"
 
-    def test_is_mycard_not_clean(self):
-        """Testing init data"""
-        mycard = Mycard.objects.get(id=1)
-        # Is mycard not clean
-        self.assertIsNotNone(mycard)
-
-    def test_is_init_data_correct(self):
-        """Testing correcting of init data"""
+    def test__is_init_data_correct(self):
+        """Testing of init data accuracy"""
         # Is All properties included
         mycard = Mycard.objects.get(id=1)
         self.assertIsNotNone(mycard)
@@ -26,7 +22,6 @@ class TestMycardModel(TestCase):
         self.assertIn('bio', mycard.__dict__)
         self.assertIn('other_contacts', mycard.__dict__)
         # Is data correct
-        self.assertEqual(mycard.skype, self.Skype_str)
         self.assertEqual(mycard.jabber, 'ykorostelyov@khavr.com')
         self.assertEqual(mycard.first_name+' '+mycard.last_name,
                          'Yuriy Korostelyov')
@@ -34,23 +29,33 @@ class TestMycardModel(TestCase):
         self.assertNotEqual(len(mycard.other_contacts), 0)
         self.assertNotEqual(mycard.birth_date, datetime.date.today())
 
-    def test_is_db_have_single_record(self):
-        """Testing db on showing only single record"""
-        mycard = Mycard.objects.filter(id=1).count()
-        # Is received some data
-        self.assertNotEqual(mycard, 0)
-        # Is received single record
-        self.assertEqual(mycard, 1)
-
-    def test_is_data_model_presented_in_db(self):
-        """Testing of site structure"""
-        # Getting default page
-        response = self.client.get('')
-        # Is current response valid
+    def test_view__unicode_characters(self):
+        """Displaying of Unicode characters"""
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        # Is used template correct
-        self.assertTemplateUsed(response, 'hello/index.html')
-        # Getting invalid page
-        response = self.client.get('invalid')
-        # Is current response invalid
-        self.assertEqual(response.status_code, 404)
+        # Unicode testing (bio, other contacts)
+        self.assertContains(response, u'Биография')
+        self.assertContains(response, u'Другие')
+
+    def test_view__raising_404_when_no_data(self):
+        """raising 404 when no_data for view"""
+        total_records_count = Mycard.objects.filter().count()
+        with self.assertRaises(Http404):
+            get_object_or_404(Mycard, pk=total_records_count+1)
+
+    def test_view__only_one_record_from_db(self):
+        """Only one record from db showing in view"""
+        Mycard.objects.create(first_name=u'Имя2',
+                              last_name=u'Фамилия2',
+                              bio=u'Био2',
+                              email=u'ykorostelyov@gmail.com',
+                              jabber=u'ykorostelyov@khavr.com',
+                              skype=u'yuriy.torhammer',
+                              birth_date=u'1983-01-13',
+                              other_contacts=u'Другие контакты2')
+
+        total_records_count = Mycard.objects.count()
+        self.assertGreaterEqual(total_records_count, 2)
+
+        response = self.client.get(reverse('home'))
+        self.assertNotEqual(response, u'Имя2')
