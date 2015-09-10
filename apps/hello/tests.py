1b08854086b257427e80f23727c8963ae548736d
ykorostelyov <ykorostelyov@gmail.com>
2015-09-10 21:25:47 +0300

    ticket:1 fixing some observations

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 from django.core.urlresolvers import reverse
-from django.shortcuts import get_object_or_404
-from django.http import Http404
 from .models import Mycard
 import datetime
 
@@ -10,9 +8,11 @@ import datetime
 class TestMycardModel(TestCase):
 
     def test__is_init_data_correct(self):
-        """Testing of init data accuracy"""
+        """
+        Testing of init data accuracy
+        """
         # Is All properties included
-        mycard = Mycard.objects.get(id=1)
+        mycard = Mycard.objects.first()
         self.assertIsNotNone(mycard)
         self.assertIn('first_name', mycard.__dict__)
         self.assertIn('last_name', mycard.__dict__)
@@ -30,7 +30,9 @@ class TestMycardModel(TestCase):
         self.assertNotEqual(mycard.birth_date, datetime.date.today())
 
     def test_view__unicode_characters(self):
-        """Displaying of Unicode characters"""
+        """
+        Displaying of Unicode characters
+        """
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         # Unicode testing (bio, other contacts)
@@ -38,13 +40,17 @@ class TestMycardModel(TestCase):
         self.assertContains(response, u'Другие')
 
     def test_view__raising_404_when_no_data(self):
-        """raising 404 when no_data for view"""
-        total_records_count = Mycard.objects.filter().count()
-        with self.assertRaises(Http404):
-            get_object_or_404(Mycard, pk=total_records_count+1)
+        """
+        raising 404 when no_data for view
+        """
+        Mycard.objects.all().delete()
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 404)
 
     def test_view__only_one_record_from_db(self):
-        """Only one record from db showing in view"""
+        """
+        Only one record from db showing in view
+        """
         Mycard.objects.create(first_name=u'Имя2',
                               last_name=u'Фамилия2',
                               bio=u'Био2',
@@ -53,9 +59,33 @@ class TestMycardModel(TestCase):
                               skype=u'yuriy.torhammer',
                               birth_date=u'1983-01-13',
                               other_contacts=u'Другие контакты2')
-
         total_records_count = Mycard.objects.count()
         self.assertGreaterEqual(total_records_count, 2)
 
         response = self.client.get(reverse('home'))
         self.assertNotEqual(response, u'Имя2')
+
+    def test_view__using_rendering_data_from_db(self):
+        """
+        Using of rendering data, not static
+        """
+        # Deleting all data to check response
+        Mycard.objects.all().delete()
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 404)
+        # Creating DB record for rendering
+        mycard = Mycard.objects.create(first_name='Имя3',
+                                       last_name='Фамилия3',
+                                       bio=u'Био3',
+                                       email=u'ykorostelyov@gmail.com',
+                                       jabber=u'ykorostelyov@khavr.com',
+                                       skype=u'yuriy.torhammer',
+                                       birth_date=u'1983-01-13',
+                                       other_contacts=u'Другие контакты3')
+
+        # calling home view with rendered data
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        # Checking of rendered data
+        self.assertEqual(mycard.__unicode__(),
+                         str(response.context['first_result']))
