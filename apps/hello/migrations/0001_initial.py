0b02a35cab57a2fa935e837b102f2752a230832d
ykorostelyov <ykorostelyov@gmail.com>
2015-10-05 13:34:29 +0300

    ticket:5 some migration changes

--- apps/hello/migrations/0001_initial.py
+++ apps/hello/migrations/0001_initial.py
@@ -8,84 +8,21 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Deleting field 'RequestInfo.fromIP'
-        db.delete_column(u'hello_requestinfo', 'fromIP')
-
-        # Deleting field 'RequestInfo.isViewed'
-        db.delete_column(u'hello_requestinfo', 'isViewed')
-
-        # Adding field 'RequestInfo.host'
-        db.add_column(u'hello_requestinfo', 'host',
-                      self.gf('django.db.models.fields.CharField')(default=1, max_length=1024),
-                      keep_default=False)
-
-        # Adding field 'RequestInfo.remote_addr'
-        db.add_column(u'hello_requestinfo', 'remote_addr',
-                      self.gf('django.db.models.fields.GenericIPAddressField')(default="127.0.0.1", max_length=39),
-                      keep_default=False)
-
-        # Adding field 'RequestInfo.is_viewed'
-        db.add_column(u'hello_requestinfo', 'is_viewed',
-                      self.gf('django.db.models.fields.BooleanField')(default=False),
-                      keep_default=False)
-
-        # Adding field 'RequestInfo.is_ajax'
-        db.add_column(u'hello_requestinfo', 'is_ajax',
-                      self.gf('django.db.models.fields.BooleanField')(default=False),
-                      keep_default=False)
-
-        # Adding field 'RequestInfo.uri'
-        db.add_column(u'hello_requestinfo', 'uri',
-                      self.gf('django.db.models.fields.CharField')(default=1, max_length=1024),
-                      keep_default=False)
-
-        # Adding field 'RequestInfo.status_code'
-        db.add_column(u'hello_requestinfo', 'status_code',
-                      self.gf('django.db.models.fields.CharField')(default=1, max_length=30),
-                      keep_default=False)
-
-        # Adding field 'RequestInfo.user_agent'
-        db.add_column(u'hello_requestinfo', 'user_agent',
-                      self.gf('django.db.models.fields.CharField')(default=1, max_length=1024),
+        # Adding field 'Mycard.avatar'
+        db.add_column(u'hello_mycard', 'avatar',
+                      self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True),
                       keep_default=False)
 
 
     def backwards(self, orm):
-        # Adding field 'RequestInfo.fromIP'
-        db.add_column(u'hello_requestinfo', 'fromIP',
-                      self.gf('django.db.models.fields.GenericIPAddressField')(default=1, max_length=39),
-                      keep_default=False)
-
-        # Adding field 'RequestInfo.isViewed'
-        db.add_column(u'hello_requestinfo', 'isViewed',
-                      self.gf('django.db.models.fields.BooleanField')(default=False),
-                      keep_default=False)
-
-        # Deleting field 'RequestInfo.host'
-        db.delete_column(u'hello_requestinfo', 'host')
-
-        # Deleting field 'RequestInfo.remote_addr'
-        db.delete_column(u'hello_requestinfo', 'remote_addr')
-
-        # Deleting field 'RequestInfo.is_viewed'
-        db.delete_column(u'hello_requestinfo', 'is_viewed')
-
-        # Deleting field 'RequestInfo.is_ajax'
-        db.delete_column(u'hello_requestinfo', 'is_ajax')
-
-        # Deleting field 'RequestInfo.uri'
-        db.delete_column(u'hello_requestinfo', 'uri')
-
-        # Deleting field 'RequestInfo.status_code'
-        db.delete_column(u'hello_requestinfo', 'status_code')
-
-        # Deleting field 'RequestInfo.user_agent'
-        db.delete_column(u'hello_requestinfo', 'user_agent')
+        # Deleting field 'Mycard.avatar'
+        db.delete_column(u'hello_mycard', 'avatar')
 
 
     models = {
         u'hello.mycard': {
             'Meta': {'object_name': 'Mycard'},
+            'avatar': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'bio': ('django.db.models.fields.TextField', [], {}),
             'birth_date': ('django.db.models.fields.DateField', [], {}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
