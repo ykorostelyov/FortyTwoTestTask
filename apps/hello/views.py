4d2321131fd69488ec8e03cac0fbbe6b82186d08
ykorostelyov <ykorostelyov@gmail.com>
2015-10-26 16:17:52 +0200

    ticket:13 Update/Select corrections

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -13,7 +13,7 @@ log = logging.getLogger('apps')
 
 
 def index(request):
-    # db no-data checking 
+    # db no-data checking
     try:
         my_data = Mycard.objects.first()
     except:
@@ -56,7 +56,7 @@ def requests_cnt(request):
 
 @login_required()
 def edit(request):
-    # no data checking
+    # db no-data checking
     try:
         edit_form = MycardForm(instance=Mycard.objects.first())
     except:
